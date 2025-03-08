8a5a49c756db0fe370fa6550667751c1dd7fd73c
Carlos Felipe Maradeia <carlos.maradeia@loft.com.br>
2025-03-07 23:06:10 -0300

    Implementando métodos do service

--- service/StockService.go
+++ service/StockService.go
@@ -1,9 +1,65 @@
 package service
 
 import (
+	"crud-go/model"
+
 	"gorm.io/gorm"
 )
 
 type StockService struct {
 	db *gorm.DB
+}
+
+func NewStockService(db *gorm.DB) *StockService {
+	return &StockService{
+		db: db,
+	}
+}
+
+func (s *StockService) FindById(id uint64) (model.Stock, error) {
+	stock := new(model.Stock)
+	resp := s.db.First(&stock, id)
+
+	if resp.Error != nil {
+		return model.Stock{}, resp.Error
+	}
+
+	return *stock, nil
+}
+
+func (s *StockService) SaveStock(stock model.Stock) (model.Stock, error) {
+	resp := s.db.Create(&stock)
+
+	if resp.Error != nil {
+		return model.Stock{}, resp.Error
+	}
+
+	return stock, nil
+}
+
+func (s *StockService) UpdateStock(stock model.Stock, id uint64) (model.Stock, error) {
+	exist := new(model.Stock)
+	result := s.db.First(&exist, id)
+
+	if result.Error != nil {
+		return model.Stock{}, result.Error
+	}
+
+	exist.Ticker = stock.Ticker
+	exist.Price = stock.Price
+	resp := s.db.Save(&exist)
+	if resp.Error != nil {
+		return model.Stock{}, resp.Error
+	}
+
+	return *exist, nil
+}
+
+func (s *StockService) DeleteById(id uint64) error {
+	result := s.db.Delete(&model.Stock{}, id)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
 }
